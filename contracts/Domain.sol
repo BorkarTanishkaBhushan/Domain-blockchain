98bd4ac3c60597da273f131b7ed91617c9c95697
Tanishka Borkar <tanishka.b@somaiya.edu>
2022-06-21 12:05:45 -0200

    added price function and modified register function accordingly

--- contracts/Domain.sol
+++ contracts/Domain.sol
@@ -2,18 +2,43 @@
 
 pragma solidity ^0.8.10;
 
+import {StringUtils} from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains{
+    //Top-level-domain(tld)
+    string public tld;
+
     mapping(string => address) public domains; //for storing addresses
     mapping (string => string) public records; //for storing records
-    constructor(){
-        console.log("This is my domain contract and it is running");
+    
+    //made the constructor payable
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log("%s name service deployed", _tld);
+    }
+
+    //this gives the price of the domain depending on the lenght
+    function price(string calldata name) public pure returns(uint){
+        uint len = StringUtils.strlen(name);
+        require(len > 0);
+        if(len == 3)
+        {
+            return 5 * 10**17;
+        }
+        else if(len == 4) {
+            return 3 * 10**17;
+        }
+        else{
+            return 1 * 10**17;
+        }
     }
 
     //this function will map the domain names with the owners address
     function register(string calldata name) public {
         require(domains[name] == address(0)); //checks whether the domain is not taken by someone else
+        uint _price = price(name);
+        require(msg.value >= _price, "Not enough Matic paid");
         domains[name] = msg.sender;
         console.log("%s has regitered a domain!", msg.sender);
     }
