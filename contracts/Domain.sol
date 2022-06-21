c8e956ccaf0b89ea8e29c8ccfca36aa235f3a8cf
Tanishka Borkar <tanishka.b@somaiya.edu>
2022-06-21 04:36:00 -0200

    added two simple functions in the contract

--- contracts/Domain.sol
+++ contracts/Domain.sol
@@ -5,7 +5,18 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains{
+    mapping(string => address) public domains;
+
     constructor(){
         console.log("This is my domain contract and it is running");
+    }
+
+    function register(string calldata name) public {
+        domains[name] = msg.sender;
+        console.log("%s has regitered a domain!", msg.sender);
+    }
+
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
     }
 }
